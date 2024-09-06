bf545eb54e638c052f52c22839a0d29d760f93be
Donald Zar <177225530+donniecode@users.noreply.github.com>
2024-09-06 22:00:18 +0200

    Add files via upload

--- contact.js
+++ contact.js
@@ -12,3 +12,37 @@ closemenu.addEventListener('click', ()=>{
  closemenu.style.display='none';
  sidebar.style.display='none';
 })
+
+
+/* form smtpjs */
+
+const sumbitBtn = document.querySelector('#submit-btn');
+sumbitBtn.addEventListener('click', function(e){
+    e.preventDefault();
+
+let namevalue = document.querySelector('#name').value;
+let emailvalue = document.querySelector('#email').value;
+let phonevalue = document.querySelector('#phone').value;
+let messagevalue = document.querySelector('#message').value;
+
+let body = `Name: ` + namevalue + `<br/> Email: ` + emailvalue + `<br/>Phone: ` + phonevalue + `<br/> Message: ` + messagevalue;
+
+    Email.send({
+Host : "smtp.elasticemail.com",
+Username : "doniecode@gmail.com",
+Password : "BA4BC34BD08DAED7EB189823D322B358CDB1",
+To : 'doniecode@gmail.com',
+From : "doniecode@gmail.com",
+Subject : "Msg From Mount Vista Website",
+Body : body,
+}).then(
+message => {
+    if(message=='OK'){
+        swal("Thank You!", "Message sent. Mount Vista will reply ASAP", "success");
+    } else{
+        swal ( "Oops" ,  "Something went wrong!" ,  "error" );
+    }
+}
+);
+
+})
